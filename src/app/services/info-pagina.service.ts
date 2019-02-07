2f9e6ccd58c30fea6d798a6f68d4d32a8e2307aa
Danny Garzón <dkmetalcol@misena.edu.co>
2019-02-07 14:16:10 -0500

    Fin de la sección 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,17 +9,34 @@ export class InfoPaginaService {
   
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
   constructor( private http: HttpClient ) {
-    console.log('Servicio de infoPagina cargado');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
+    //console.log('Servicio de infoPagina cargado');
     
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json').subscribe(
       (resp: InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
+        //console.log(resp);
         //console.log(resp['email']);
       });
   }
+
+  private cargarEquipo(){
+    this.http.get('https://angular-html-d3555.firebaseio.com/equipo.json').subscribe(
+      (resp: any[]) => {
+        
+        this.equipo = resp;
+        //console.log(resp);
+        //console.log(resp['0']['nombre']);
+      });
+  }
+
 }
